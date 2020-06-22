28c6cbeabbbf5eb4aa79ca66bd5d24958ac21b35
gemer <gemercheung@gmail.com>
2020-06-22 17:55:48 +0800

    first time add formula

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# webos-ares-cli
+# A LG TV ares-cli formula for brew
+
+for command with brew:
+
+```shell
+brew tap Gemerz/webos-ares-cli
+brew install ares-cli
+```
---
+++ ares-cli.rb
@@ -0,0 +1,20 @@
+class AresCli < Formula
+    desc "The LG tv ares cli for macOs."
+    homepage "https://github.com/Gemerz/webos-ares-cli/"
+    url "https://github.com/Gemerz/webos-ares-cli/releases/download/1.11.1/ares-webos-cli-mac-1.11.1.tgz"
+    sha256 "9ea1b103b24e5c05bc74c3d72a4d9b0b11eff62dcef70c992de349e52a3d305e"
+  
+    bottle :unneeded
+    depends_on "node" => :build
+  
+    def install
+      libexec.install Dir["*"]
+      bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+  
+    test do
+      system bin/"ares-shell"
+      # system "false"
+    end
+  end
+  
