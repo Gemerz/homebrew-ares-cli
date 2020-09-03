e3c6d985b688892f0f2b704168b1952a311f0901
gemer <gemercheung@gmail.com>
2020-09-03 18:14:44 +0800

    更改1.11.1的homepage

--- ares-cli@1.11.1.rb
+++ ares-cli@1.11.1.rb
@@ -1,9 +1,8 @@
 class AresCli < Formula
-    desc "The LG tv ares cli for macOs."
-    homepage "https://github.com/Gemerz/webos-ares-cli/"
+    desc "The LG tv ares cli for macOs"
+    homepage "https://github.com/Gemerz/homebrew-ares-cli"
     url "https://github.com/Gemerz/homebrew-ares-cli/releases/download/1.11.1/ares-webos-cli-mac-1.11.1.tgz"
     sha256 "9ea1b103b24e5c05bc74c3d72a4d9b0b11eff62dcef70c992de349e52a3d305e"
-  
     bottle :unneeded
     keg_only :versioned_formula
     depends_on "node" => :build
