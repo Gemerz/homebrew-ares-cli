a23d7e3e4415d6a29ad64d33d7f098ad9795db4c
gemer <gemercheung@gmail.com>
2020-09-03 18:10:43 +0800

    与LG官方同步，默认是1.10.4版本

--- ares-cli.rb
+++ ares-cli.rb
@@ -1,9 +1,8 @@
-class AresCli < Formula
-    desc "The LG tv ares cli for macOs."
-    homepage "https://github.com/Gemerz/webos-ares-cli/"
-    url "https://github.com/Gemerz/homebrew-ares-cli/releases/download/1.11.1/ares-webos-cli-mac-1.11.1.tgz"
-    sha256 "9ea1b103b24e5c05bc74c3d72a4d9b0b11eff62dcef70c992de349e52a3d305e"
-  
+class AresCliAT1104 < Formula
+    desc "The LG tv ares cli for macOs"
+    homepage "https://github.com/Gemerz/homebrew-ares-cli"
+    url "https://github.com/Gemerz/homebrew-ares-cli/releases/download/1.10.4/ares-webos-cli-mac-1.10.4.tgz"
+    sha256 "11e5df6753e07bdfd53445704c820d14ca0119d184449929f84ac3fa0b13e7a3"
     bottle :unneeded
     depends_on "node" => :build
   
