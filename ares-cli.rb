2f8c4df3b10eda53368d562608489e5bd5587169
gemer <gemercheung@gmail.com>
2020-09-07 12:24:35 +0800

    broke 1.10.4 to lg ose 1.10.2

--- ares-cli.rb
+++ ares-cli.rb
@@ -1,8 +1,8 @@
  class AresCli < Formula
     desc "The LG tv ares cli for macOs"
     homepage "https://github.com/Gemerz/homebrew-ares-cli"
-    url "https://github.com/Gemerz/homebrew-ares-cli/releases/download/1.10.4/ares-webos-cli-mac-1.10.4.tgz"
-    sha256 "11e5df6753e07bdfd53445704c820d14ca0119d184449929f84ac3fa0b13e7a3"
+    url "https://github.com/Gemerz/homebrew-ares-cli/releases/download/1.10.2/ares-webos-cli-mac-1.10.2.tgz"
+    sha256 "d3a91150e0d3dd18c2044c82eb368f47b3cd964b1999fc27ad10e5efc6414a6a"
     bottle :unneeded
     depends_on "node" => :build
   
